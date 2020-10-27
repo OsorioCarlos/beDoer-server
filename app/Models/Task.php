450996ecec61ccf8aa4ce0f805061a6881f7b1ac
JoelTapia99 <joelts159@gmail.com>
2020-10-27 10:40:13 -0500

    create relations Eloquent

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(Member::class);
+    }
+
+    public function state(){
+        return $this->hasOne(State::class);
+    }
+
+    public function team(){
+        return $this->belongsTo(Team::class);
+    }
+
 }
