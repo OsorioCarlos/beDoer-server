f0b672f2e3f002ce14d2a2f9b2c145d586f880cd
OsorioCarlos <carlosalexander.2001@hotmail.com>
2021-03-22 18:14:28 -0500

    CategoryFactory, UserFactory, Routes: teams, categories

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\TeamController;
+// Laravel
 use Brick\Math\Exception\RoundingNecessaryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Controllers
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +26,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/* Route::apiResource('team', TeamController::class); */
+Route::apiResource('teams', TeamController::class);
+
+Route::apiResource('categories', CategoryController::class);
+
 /* Rutas Task */
 Route::get("/tasks", [TaskController::class, 'index']);
-
-/* Rutas Team */
-Route::get("/teams", [TeamController::class, 'index']);
 
 /* Rutas Role */
 
